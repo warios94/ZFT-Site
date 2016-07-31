09ae23cb95a5e7b8ee0488b39ae8a0c226eb4ee8
warios94 <warios49@gmail.com>
2016-07-31 22:33:45 +0200

    Primo commit

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Users Main Page</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/newStyle.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+
+<body class="body">
+
+<header class="mainHeader">
+    <input type="checkbox" id="resizeMenu">
+    <label for="resizeMenu"><img class= "buttonMenu" src="images/menu.png" alt=""></label>
+
+ <img src="images/logo.png" id="logo_resize">
+
+<nav class="menu">
+<ul class="logo">
+<li><img  src="images/logo.png" id="logo_fisso"></li>
+</ul>
+	<ul>
+	    <li class="active"><a href="#">Home</a></li>
+		<li><a href="index.html">Servizi</a></li>
+		<li><a href="#">Area Clienti</a></li>
+		</ul>
+</nav>
+</header>
+<div class="mainContent">
+		<div class="content">
+				<article class="topcontent">
+					<header>
+						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">Second article</a></h2>
+					</header>
+
+					<footer>
+						<p class="post-info">This was posted on the 2nd of March 2013 by Christian Vasile</p>
+					</footer>
+
+					<content>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quaslj,cvaòVDUis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+						<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+					</content>
+
+				</article>
+				<article class="bottomcontent">
+					<header>
+						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">First post</a></h2>
+					</header>
+
+					<footer>
+						<p class="post-info">This was posted on the 5th of March 2013 by Christian Vasile</p>
+					</footer>
+
+										<content>
+						<ul class="slides">
+    <input type="radio" name="radio-btn" id="img-1" checked />
+    <li class="slide-container">
+		<div class="slide">
+			<img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-6" class="prev">&#x2039;</label>
+			<label for="img-2" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <input type="radio" name="radio-btn" id="img-2" />
+    <li class="slide-container">
+        <div class="slide">
+          <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-1" class="prev">&#x2039;</label>
+			<label for="img-3" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <input type="radio" name="radio-btn" id="img-3" />
+    <li class="slide-container">
+        <div class="slide">
+          <img src="http://farm9.staticflickr.com/8068/8250438572_d1a5917072_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-2" class="prev">&#x2039;</label>
+			<label for="img-4" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <input type="radio" name="radio-btn" id="img-4" />
+    <li class="slide-container">
+        <div class="slide">
+          <img src="http://farm9.staticflickr.com/8061/8237246833_54d8fa37f0_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-3" class="prev">&#x2039;</label>
+			<label for="img-5" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <input type="radio" name="radio-btn" id="img-5" />
+    <li class="slide-container">
+        <div class="slide">
+          <img src="http://farm9.staticflickr.com/8055/8098750623_66292a35c0_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-4" class="prev">&#x2039;</label>
+			<label for="img-6" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <input type="radio" name="radio-btn" id="img-6" />
+    <li class="slide-container">
+        <div class="slide">
+          <img src="http://farm9.staticflickr.com/8195/8098750703_797e102da2_z.jpg" />
+        </div>
+		<div class="nav">
+			<label for="img-5" class="prev">&#x2039;</label>
+			<label for="img-1" class="next">&#x203a;</label>
+		</div>
+    </li>
+
+    <li class="nav-dots">
+      <label for="img-1" class="nav-dot" id="img-dot-1"></label>
+      <label for="img-2" class="nav-dot" id="img-dot-2"></label>
+      <label for="img-3" class="nav-dot" id="img-dot-3"></label>
+      <label for="img-4" class="nav-dot" id="img-dot-4"></label>
+      <label for="img-5" class="nav-dot" id="img-dot-5"></label>
+      <label for="img-6" class="nav-dot" id="img-dot-6"></label>
+    </li>
+</ul>
+					</content>
+
+				</article>
+		</div>
+</div>
+</body>
+<footer class="footer">
+    <div class="mainFooter">
+    <nav>
+        <ul>
+            <li><p>Copyright &copy; 2016 <a href="#">ZFT</a></p></li>
+        </ul>
+    </nav>
+
+	</div>
+</footer>
+</html>
