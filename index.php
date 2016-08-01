c0e4872b21fa64733cb8e9934873b3ba03e2cac2
d9c4 <davide.cifariello@gmail.com>
2016-08-01 19:07:10 +0200

    prova1

--- index.php
+++ index.php
@@ -1,149 +1,107 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Users Main Page</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="CSS/newStyle.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
 
+<head>
+    <title>Users Main Page</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="CSS/newStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> </head>
 
 <body class="body">
-
-<header class="mainHeader">
-    <input type="checkbox" id="resizeMenu">
-    <label for="resizeMenu"><img class= "buttonMenu" src="images/menu.png" alt=""></label>
-
- <img src="images/logo.png" id="logo_resize">
-
-<nav class="menu">
-<ul class="logo">
-<li><img  src="images/logo.png" id="logo_fisso"></li>
-</ul>
-	<ul>
-	    <li class="active"><a href="#">Home</a></li>
-		<li><a href="index.html">Servizi</a></li>
-		<li><a href="#">Area Clienti</a></li>
-		</ul>
-</nav>
-</header>
-<div class="mainContent">
-		<div class="content">
-				<article class="topcontent">
-					<header>
-						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">Second article</a></h2>
-					</header>
-
-					<footer>
-						<p class="post-info">This was posted on the 2nd of March 2013 by Christian Vasile</p>
-					</footer>
-
-					<content>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quaslj,cvaòVDUis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-						<p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
-					</content>
-
-				</article>
-				<article class="bottomcontent">
-					<header>
-						<h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">First post</a></h2>
-					</header>
-
-					<footer>
-						<p class="post-info">This was posted on the 5th of March 2013 by Christian Vasile</p>
-					</footer>
-
-										<content>
-						<ul class="slides">
-    <input type="radio" name="radio-btn" id="img-1" checked />
-    <li class="slide-container">
-		<div class="slide">
-			<img src="http://farm9.staticflickr.com/8072/8346734966_f9cd7d0941_z.jpg" />
+    <header class="mainHeader">
+        <input type="checkbox" id="resizeMenu">
+        <label for="resizeMenu"><img class="buttonMenu" src="images/menu.png" alt=""></label> <img src="images/logo.png" id="logo_resize">
+        <nav class="menu">
+            <ul class="logo">
+                <li><img src="images/logo.png" id="logo_fisso"></li>
+            </ul>
+            <ul>
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="index.html">Servizi</a></li>
+                <li><a href="#">Area Clienti</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="mainContent">
+        <div class="content">
+            <article class="topcontent">
+                <header>
+                    <h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">Second article</a></h2> </header>
+                <footer>
+                    <p class="post-info">This was posted on the 2nd of March 2013 by Christian Vasile</p>
+                </footer>
+                <content>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quaslj,cvaòVDUis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio.</p>
+                </content>
+            </article>
+            <article class="slideContent">
+                <header>
+                    <h2><a href="#" rel="bookmark" title="Permalink to this POST TITLE">First post</a></h2> </header>
+                <footer>
+                    <p class="post-info">This was posted on the 5th of March 2013 by Christian Vasile</p>
+                </footer>
+                <content>
+                    <!--SLIDER -->
+                    <!-- Start cssSlider.com -->
+                    <div class='csslider1 autoplay '>
+                        <input name="cs_anchor1" id='cs_slide1_0' type="radio" class='cs_anchor slide'>
+                        <input name="cs_anchor1" id='cs_slide1_1' type="radio" class='cs_anchor slide'>
+                        <input name="cs_anchor1" id='cs_slide1_2' type="radio" class='cs_anchor slide'>
+                        <input name="cs_anchor1" id='cs_play1' type="radio" class='cs_anchor' checked>
+                        <input name="cs_anchor1" id='cs_pause1' type="radio" class='cs_anchor'>
+                        <ul>
+                            <div style="width: 100%; visibility: hidden; font-size: 0px; line-height: 0;"> <img src="http://cssslider.com/sliders/pen/images/buns.jpg" style="width: 100%;"> </div>
+                            <li class='num0 img'>
+                                <a href="http://cssslider.com" target=""><img src='http://cssslider.com/sliders/pen/images/buns.jpg' alt='Buns' title='Buns' /> </a>
+                            </li>
+                            <li class='num1 img'>
+                                <a href="http://cssslider.com" target=""><img src='http://cssslider.com/sliders/pen/images/buns.jpg' alt='Croissant' title='Croissant' /> </a>
+                            </li>
+                            <li class='num2 img'>
+                                <a href="http://cssslider.com" target=""><img src='http://cssslider.com/sliders/pen/images/buns.jpg' alt='Lemon pie' title='Lemon pie' /> </a>
+                            </li>
+                        </ul>
+                        <div class='cs_description'>
+                            <label class='num0'> <span class="cs_title"><span class="cs_wrapper">Buns</span></span>
+                            </label>
+                            <label class='num1'> <span class="cs_title"><span class="cs_wrapper">Croissant</span></span>
+                            </label>
+                            <label class='num2'> <span class="cs_title"><span class="cs_wrapper">Lemon pie</span></span>
+                            </label>
+                        </div>
+                        <div class='cs_arrowprev'>
+                            <label class='num0' for='cs_slide1_0'></label>
+                            <label class='num1' for='cs_slide1_1'></label>
+                            <label class='num2' for='cs_slide1_2'></label>
+                        </div>
+                        <div class='cs_arrownext'>
+                            <label class='num0' for='cs_slide1_0'></label>
+                            <label class='num1' for='cs_slide1_1'></label>
+                            <label class='num2' for='cs_slide1_2'></label>
+                        </div>
+                        <div class='cs_bullets'>
+                            <label class='num0' for='cs_slide1_0'> <span class='cs_point'></span> <span class='cs_thumb'><img src='http://cssslider.com/sliders/pen/tooltips/buns.jpg' alt='Buns' title='Buns' /></span> </label>
+                            <label class='num1' for='cs_slide1_1'> <span class='cs_point'></span> <span class='cs_thumb'><img src='http://cssslider.com/sliders/pen/tooltips/croissant.jpg' alt='Croissant' title='Croissant' /></span> </label>
+                            <label class='num2' for='cs_slide1_2'> <span class='cs_point'></span> <span class='cs_thumb'><img src='http://cssslider.com/sliders/pen/tooltips/lemonpie.jpg' alt='Lemon pie' title='Lemon pie' /></span> </label>
+                        </div>
+                    </div>
+                    <!-- End cssSlider.com -->
+                </content>
+            </article>
         </div>
-		<div class="nav">
-			<label for="img-6" class="prev">&#x2039;</label>
-			<label for="img-2" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <input type="radio" name="radio-btn" id="img-2" />
-    <li class="slide-container">
-        <div class="slide">
-          <img src="http://farm9.staticflickr.com/8504/8365873811_d32571df3d_z.jpg" />
-        </div>
-		<div class="nav">
-			<label for="img-1" class="prev">&#x2039;</label>
-			<label for="img-3" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <input type="radio" name="radio-btn" id="img-3" />
-    <li class="slide-container">
-        <div class="slide">
-          <img src="http://farm9.staticflickr.com/8068/8250438572_d1a5917072_z.jpg" />
-        </div>
-		<div class="nav">
-			<label for="img-2" class="prev">&#x2039;</label>
-			<label for="img-4" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <input type="radio" name="radio-btn" id="img-4" />
-    <li class="slide-container">
-        <div class="slide">
-          <img src="http://farm9.staticflickr.com/8061/8237246833_54d8fa37f0_z.jpg" />
-        </div>
-		<div class="nav">
-			<label for="img-3" class="prev">&#x2039;</label>
-			<label for="img-5" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <input type="radio" name="radio-btn" id="img-5" />
-    <li class="slide-container">
-        <div class="slide">
-          <img src="http://farm9.staticflickr.com/8055/8098750623_66292a35c0_z.jpg" />
-        </div>
-		<div class="nav">
-			<label for="img-4" class="prev">&#x2039;</label>
-			<label for="img-6" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <input type="radio" name="radio-btn" id="img-6" />
-    <li class="slide-container">
-        <div class="slide">
-          <img src="http://farm9.staticflickr.com/8195/8098750703_797e102da2_z.jpg" />
-        </div>
-		<div class="nav">
-			<label for="img-5" class="prev">&#x2039;</label>
-			<label for="img-1" class="next">&#x203a;</label>
-		</div>
-    </li>
-
-    <li class="nav-dots">
-      <label for="img-1" class="nav-dot" id="img-dot-1"></label>
-      <label for="img-2" class="nav-dot" id="img-dot-2"></label>
-      <label for="img-3" class="nav-dot" id="img-dot-3"></label>
-      <label for="img-4" class="nav-dot" id="img-dot-4"></label>
-      <label for="img-5" class="nav-dot" id="img-dot-5"></label>
-      <label for="img-6" class="nav-dot" id="img-dot-6"></label>
-    </li>
-</ul>
-					</content>
-
-				</article>
-		</div>
-</div>
+    </div>
 </body>
 <footer class="footer">
     <div class="mainFooter">
-    <nav>
-        <ul>
-            <li><p>Copyright &copy; 2016 <a href="#">ZFT</a></p></li>
-        </ul>
-    </nav>
-
-	</div>
+        <nav>
+            <ul>
+                <li>
+                    <p>Copyright &copy; 2016 <a href="#">ZFT</a></p>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </footer>
+
 </html>
